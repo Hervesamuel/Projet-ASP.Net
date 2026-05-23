0d967b7b7915b0b0fe2769800e016a7f009aac58
hervesamuel <herveandrianomena@gmail.com>
2026-05-23 15:14:17 +0300

    Netoyer .vs + git ignore

--- GestionEmploiTemps.API/GestionEmploiTemps.API/Data/AppBdContext.cs
+++ GestionEmploiTemps.API/GestionEmploiTemps.API/Data/AppBdContext.cs
@@ -9,13 +9,14 @@ namespace GestionEmploiTemps.API.Data
         {
         }
 
+        // Chaque DbSet = table dans la base de données
         public DbSet<Niveau> Niveaux { get; set; }
         public DbSet<Parcours> Parcours { get; set; }
         public DbSet<Enseignant> Enseignants { get; set; }
         public DbSet<Matiere> Matieres { get; set; }
         public DbSet<Salle> Salles { get; set; }
         public DbSet<Creneau> Creneaux { get; set; }
-        public DbSet<Enseigner> Enseigners { get; set; }
+        public DbSet<Enseigner> Enseignements { get; set; }
         public DbSet<Seance> Seances { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -23,8 +24,16 @@ namespace GestionEmploiTemps.API.Data
             base.OnModelCreating(modelBuilder); //ModelBuilder = configuration avancée EF Core
 
             modelBuilder.Entity<Enseigner>()
-                //.HasKey(...) = définit la clé primaire
+                //.HasKey(...) = définit la clé primaire , exemple : un enseignant + une matière = unique composée
                 .HasKey(e => new { e.IdEns, e.IdMatiere });
+
+
+            // Clé composite pour Enseigner
+            modelBuilder.Entity<Seance>()
+                .HasOne(s => s.Enseignant)
+                .WithMany()
+                .HasForeignKey(s => s.IdEns);
+
         }
     }
 }
